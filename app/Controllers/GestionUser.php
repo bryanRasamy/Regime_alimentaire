78a5a5c5b6d3a0d9799f1cc72cbeb921f85288ab
RASAMIARAMANANA Bryan <bryanherilalaina@gmail.com>
2026-05-06 23:05:35 +0300

    04

--- app/Controllers/GestionUser.php
+++ app/Controllers/GestionUser.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
+
 class GestionUser extends BaseController{
     public function index(){
         $data = [
@@ -9,5 +11,26 @@ class GestionUser extends BaseController{
         ];
 
         return view('login', $data);
+    }
+
+    public function authentifier(){
+        $userModel = new UserModel();
+        $nom = $this->request->getPost('nom');
+        $mot_de_passe = $this->request->getPost('password');
+
+        $user = $userModel->where('nom', $nom)->first();
+
+        if ($user && password_verify($mot_de_passe, $user['password'])) {
+            session()->set('user', [
+                'id' => $user['id_user'],
+                'nom' => $user['nom'],
+                'email' => $user['email'],
+                'id_statut' => $user['id_statut']
+            ]);
+
+            return redirect()->to('/home')->with('success', 'Connexion réussie');
+        } else {
+            return redirect()->back()->withInput()->with('error', 'nom ou mot de passe incorrect');
+        }
     }
 }
---
+++ app/Controllers/RoleFilter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+use CodeIgniter\Filters\FilterInterface;
+
+class RoleFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null){
+        $session = session();
+        $user = $session->get('user');
+        $arguments=[1];
+        
+        if (!$user || !in_array($user['id_statut'], $arguments ?? [])) {
+            return redirect()->back()->with('error', 'Accès refusé : droits insuffisants');
+        }
+    }
+    
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null){
+        
+    }
+}
